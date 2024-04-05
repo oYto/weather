fd6fb793d209e154d730be81f03c89ee86908009
oYto <1609268031@qq.com>
2024-04-06 00:05:17 +0800

    Finished 1.0

--- weather-master/weather-information-service/internal/service/serivce.go
+++ weather-master/weather-information-service/internal/service/serivce.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"weatherinfo/internal/util"
 	"weatherinfo/pkg/pb"
 )
 
@@ -18,7 +19,6 @@ type WeatherService struct {
 }
 
 func (w WeatherService) GetCurrentWeather(ctx context.Context, req *pb.GetCurrentWeatherRequest) (*pb.GetCurrentWeatherResponse, error) {
-
 	var cityClient pb.CityManagementServiceClient
 	conn, err := grpc.Dial("127.0.0.1:8001", grpc.WithInsecure())
 	if err != nil {
@@ -33,47 +33,15 @@ func (w WeatherService) GetCurrentWeather(ctx context.Context, req *pb.GetCurren
 	}
 	log.Println("-------------查询到城市信息", responseCity.Cities[0].Name, responseCity.Cities[0].Country,
 		responseCity.Cities[0].Uuid, responseCity.Cities[0].Latitude, responseCity.Cities[0].Longitude)
-	// 用您的 OpenWeatherMap API密钥替换YOUR_API_KEY
-	apiKey := "0da43e42e7f5c98b44c66e6a2eafa0e8"
-	// 构建请求URL
-	url := fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%f&lon=%f&exclude=minutely,hourly,daily,alerts&appid=%s&units=metric",
-		responseCity.Cities[0].Latitude, responseCity.Cities[0].Longitude, apiKey)
 
-	// 发送请求
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// 解析响应
-	var data map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
-		return nil, err
-	}
-
-	// 从响应中提取天气信息并构建响应
-	main, ok := data["main"].(map[string]interface{})
-	if !ok {
-		return nil, errors.New("invalid weather data")
-	}
-	weather := data["weather"].([]interface{})[0].(map[string]interface{})
-	condition := weather["main"].(string)
-
-	temp, _ := main["temp"].(float64)
-	humidity, _ := main["humidity"].(float64)
-	wind := data["wind"].(map[string]interface{})
-	windSpeed, _ := wind["speed"].(float64)
-
-	response := &pb.GetCurrentWeatherResponse{
+	response := pb.GetCurrentWeatherResponse{
 		CityName:    req.CityName,
-		Temperature: temp,
-		Humidity:    humidity,
-		Condition:   condition,
-		WindSpeed:   windSpeed,
+		Temperature: util.GetRandFloat64(), // Convert Kelvin to Celsius
+		Humidity:    util.GetRandFloat64(),
+		Condition:   util.GetRandomString(), // Taking the first weather condition for simplicity
+		WindSpeed:   util.GetRandFloat64(),
 	}
-
-	return response, nil
+	return &response, nil
 }
 
 func (w WeatherService) GetWeatherForecast(ctx context.Context, req *pb.GetWeatherForecastRequest) (*pb.GetWeatherForecastResponse, error) {
--- weather-master/weather-information-service/internal/util/util.go
+++ weather-master/weather-information-service/internal/util/util.go
@@ -1,1 +1,21 @@
 package util
+
+import (
+	"golang.org/x/exp/rand"
+	"time"
+)
+
+func GetRandFloat64() float64 {
+	var min, max float64
+	min, max = 0, 50
+	source := rand.NewSource(uint64(time.Now().UnixNano()))
+	random := rand.New(source)
+	return min + random.Float64()*(max-min)
+}
+
+func GetRandomString() string {
+	strings := []string{"良好", "恶劣", "不错", "优质", "很差"}
+	rand.Seed(uint64(time.Now().UnixNano()))
+	index := rand.Intn(len(strings))
+	return strings[index]
+}
